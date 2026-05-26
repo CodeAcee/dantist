4fad758ad762a31dc674ecdf86a7d02457de548f
Yaroslav Pikiner <yaroslav.pikiner@freshcodeit.com>
2026-05-26 13:36:46 +0300

    fix: switch to static output for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import node from "@astrojs/node";
 
 export default defineConfig({
-  base: "/dantist/",
-  site: "https://CodeAcee.github.io/dantist",
+  base: '/dantist/',
+  site: 'https://CodeAcee.github.io/dantist',
   integrations: [react()],
-  output: "hybrid",
-  adapter: node({ mode: "standalone" }),
+  output: 'static',
 });
