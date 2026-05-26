3f0bf86736ab3123f87430ecd62316fc3c0fed5e
Yaroslav Pikiner <yaroslav.pikiner@freshcodeit.com>
2026-05-26 15:59:28 +0300

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  base: "/dantist/",
-  site: "https://CodeAcee.github.io/dantist",
   integrations: [react()],
-  output: "static",
+
+  site: "https://codeacee.github.io",
+  base: "/dantist",
 });
