cea29ad891225fbccf4544917e01925c158daecc
Yaroslav Pikiner <yaroslav.pikiner@freshcodeit.com>
2026-05-26 13:08:59 +0300

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import node from '@astrojs/node';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import node from "@astrojs/node";
 
 export default defineConfig({
-  site: 'https://naturasmile.ua',
+  base: "/dantist/",
+  site: "https://CodeAcee.github.io/dantist",
   integrations: [react()],
-  output: 'hybrid',
-  adapter: node({ mode: 'standalone' }),
+  output: "hybrid",
+  adapter: node({ mode: "standalone" }),
 });
