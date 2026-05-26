1eb530c0461dc243e8ea72158ebda2a551bac1fd
Yaroslav Pikiner <yaroslav.pikiner@freshcodeit.com>
2026-05-26 14:05:48 +0300

    Temporarily disable API route for static GitHub Pages build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  base: '/dantist/',
-  site: 'https://CodeAcee.github.io/dantist',
+  base: "/dantist/",
+  site: "https://CodeAcee.github.io/dantist",
   integrations: [react()],
-  output: 'static',
+  output: "static",
 });
